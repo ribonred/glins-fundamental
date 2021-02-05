74f177d9b0b0a3ec1c7cd7fa5d1a94bca20472a5
redloratech <rede.akbar@loratechai.com>
2021-02-05 21:27:32 +0700

    test staging

--- case_one.js
+++ case_one.js
@@ -1,7 +1,16 @@
-let people = ["Greg", "Mary", "Devon", "James"]
+let people  = ["Greg", "Mary", "Devon", "James"] //  <= ini variabel
+// variabel people  menyimpan array
+// cara mengakses index di dalam array
+// tanda [] ini untuk memanggil index nomor berapa contoh  people[0] sama dengan Greg
 
-function removeGreg(people){
+console.log(people[0])
 
+// kita mau loop yang ada di variable tersebut
+for (var i=0;i < people.lenght;i++){ // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
+        console.log(i) // yang aku print di terminal itu i
 }
+
+
+
 
 
