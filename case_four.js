c47ce414ba4516d287307d9fece6a607ec039947
redloratech <rede.akbar@loratechai.com>
2021-05-04 21:28:04 +0700

    new readme md

---
+++ case_four.js
@@ -0,0 +1,13 @@
+let namesAndHobbies = {
+    'elie': "JavaScript",
+    'matt': "jogging",
+    'janey': "table building",
+    'tim': "sailing"
+}
+
+export {namesAndHobbies}
+
+// module.exports ={
+//     namesAndHobbies
+// }
+ 
