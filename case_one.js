feefc7227208b1a0da1c67250684dacc6052f111
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2021-05-04 22:18:29 +0800

    test pus

--- case_one.js
+++ case_one.js
@@ -1,14 +1,41 @@
-let people  = ["Greg", "Mary", "Devon", "James"] //  <= ini variabel
+let people = ["Greg", "Mary", "Devon", "James"]; //  <= ini variabel
 // variabel people  menyimpan array
 // cara mengakses index di dalam array
 // tanda [] ini untuk memanggil index nomor berapa contoh  people[0] sama dengan Greg
 
-console.log(people[0])
+// console.log(people[0])
 
-// kita mau loop yang ada di variable tersebut
-for (var i=0;i < people.lenght;i++){ // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
-        console.log(i) // yang aku print di terminal itu i
+// // kita mau loop yang ada di variable tersebut
+// const allPeople = () => {
+//         for (let i = 0; i < people.lenght; i++) { // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
+//                 console.log(people[i]); // yang aku print di terminal itu i
+
+//         }
+// };
+
+
+//Case_1 
+//tampil semua data
+const allOrang = () => {
+        for (let i = 0; i < people.length; i++) {
+                console.log(people[i]);
+        }
 }
+allOrang();
+
+
+//cari orang kemudian menggunakan filter
+const cariOrang = (o) => {
+        const allNew = people.filter((i) => o.toUpperCase() !== i.toUpperCase());
+        console.log(allNew);
+
+}
+
+cariOrang("greg");
+
+//menghapus Greed pada Array
+people.shift();
+console.log(people);
 
 
 
