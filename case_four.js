d3474c8309337d1b92348c060f538ae928e4e5e8
goddess.guy <goddess.guy@gmail.com>
2021-05-06 01:22:06 +0800

    Tugas Case Four

--- case_four.js
+++ case_four.js
@@ -3,11 +3,18 @@ let namesAndHobbies = {
     'matt': "jogging",
     'janey': "table building",
     'tim': "sailing"
+};
+
+// output seharusnya
+for (let i = 0; i < Object.keys(namesAndHobbies).length; i++) {
+    console.log(`${Object.values(namesAndHobbies)[i]} -- ${Object.keys(namesAndHobbies)[i]}`);
 }
 
-export {namesAndHobbies}
+//tambah nama dan hobi saya
+namesAndHobbies.dewi = "traveling";
+console.log(namesAndHobbies);
 
-// module.exports ={
-//     namesAndHobbies
-// }
- 
+//keluarin object saya
+if (namesAndHobbies.dewi) {
+    console.log(`${Object.keys(namesAndHobbies)[4]} ${namesAndHobbies.dewi}`);
+}
