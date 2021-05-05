bb59c36f408e930cd718c8fea11de8a8e1ee2a13
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2021-05-06 01:18:50 +0800

    JawabanCase4

--- case_four.js
+++ case_four.js
@@ -5,9 +5,18 @@ let namesAndHobbies = {
     'tim': "sailing"
 }
 
-export {namesAndHobbies}
 
-// module.exports ={
-//     namesAndHobbies
-// }
- 
+for (let i = 0; i < Object.keys(namesAndHobbies).length; i++) {
+
+    console.log(`${Object.values(namesAndHobbies)[i]} => ${Object.keys(namesAndHobbies)[i]}`);
+
+}
+
+
+namesAndHobbies.Bagus = "bermain";
+console.log(namesAndHobbies);
+if (namesAndHobbies.Bagus) {
+    console.log(`${Object.keys(namesAndHobbies)[4]} ${namesAndHobbies.Bagus} `);
+
+}
+
