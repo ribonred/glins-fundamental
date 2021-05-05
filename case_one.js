0f1aac664818fe96e2be3db6221442dffad3d711
Vega <hello.vegaveronica@gmail.com>
2021-05-05 20:18:57 +0700

    abis nambainn //vegaveronica

--- case_one.js
+++ case_one.js
@@ -1,16 +1,14 @@
-let people  = ["Greg", "Mary", "Devon", "James"] //  <= ini variabel
+let people = ["Greg", "Mary", "Devon", "James"]; //  <= ini variabel
 // variabel people  menyimpan array
 // cara mengakses index di dalam array
 // tanda [] ini untuk memanggil index nomor berapa contoh  people[0] sama dengan Greg
 
-console.log(people[0])
+console.log(people[0]);
 
 // kita mau loop yang ada di variable tersebut
-for (var i=0;i < people.lenght;i++){ // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
-        console.log(i) // yang aku print di terminal itu i
+for (var i = 0; i < people.lenght; i++) {
+  // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
+  console.log(i); // yang aku print di terminal itu i
 }
 
-
-
-
-
+//vegaveronica
