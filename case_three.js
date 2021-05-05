707ec9febf6b21bef7ab492e33e665a1682b49f7
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2021-05-05 23:55:55 +0800

    Case_3 Sudah

--- case_three.js
+++ case_three.js
@@ -1,1 +1,17 @@
-let object = {}
+let object = {
+    firstName: "BagusMiftah",
+    lastName: "Rizqullah",
+    occupation: "Programmer"
+}
+
+
+
+console.log(object.firstName);
+console.log(object.lastName);
+console.log(object.occupation);
+delete object.occupation;
+console.log(object)
+object.hobby = "bermain";
+console.log(object)
+
+
