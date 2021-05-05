db5ba3f9be138eb2d57c3017e98cf19c72047615
hematenergi <danyarkham@gmail.com>
2021-05-05 22:52:33 +0700

    upload case 3

--- case_three.js
+++ case_three.js
@@ -1,1 +1,10 @@
-let object = {}
+let akuAdalah = {
+  firstName: "dany",
+  lastName: "arkham",
+  occupation: "kang ngupi",
+};
+console.log(akuAdalah);
+delete akuAdalah.occupation; //delete occupation key
+console.log(akuAdalah);
+akuAdalah.hobby = "gitaran"; //add hobby key
+console.log(akuAdalah);
