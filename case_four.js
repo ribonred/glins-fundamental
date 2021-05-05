9eef6dbdb0fee8e6d79d3a18e79115c910cde2d8
hematenergi <danyarkham@gmail.com>
2021-05-06 00:16:01 +0700

    upload case 4

--- case_four.js
+++ case_four.js
@@ -1,13 +1,24 @@
 let namesAndHobbies = {
-    'elie': "JavaScript",
-    'matt': "jogging",
-    'janey': "table building",
-    'tim': "sailing"
+  elie: "JavaScript",
+  matt: "jogging",
+  janey: "table building",
+  tim: "sailing",
+};
+
+//Output should be:
+for (let i = 0; i < Object.keys(namesAndHobbies).length; i++) {
+  console.log(
+    `${Object.values(namesAndHobbies)[i]} => ${Object.keys(namesAndHobbies)[i]}`
+  );
 }
 
-export {namesAndHobbies}
+// add key untuk nama dany
+namesAndHobbies.dany = "bootcamp";
+console.log(namesAndHobbies);
 
-// module.exports ={
-//     namesAndHobbies
-// }
- 
+//jika key nama dany ada maka tampilkan nama dan hobby
+if (namesAndHobbies.dany) {
+  console.log(
+    `${Object.keys(namesAndHobbies)[4]} hobinya ${namesAndHobbies.dany}`
+  );
+}
