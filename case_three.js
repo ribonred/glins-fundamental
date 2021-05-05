329a445a0ddd4798c7eac8c15b2db43702d28e6b
goddess.guy <goddess.guy@gmail.com>
2021-05-05 23:53:50 +0800

    Tugas Case 3

--- case_three.js
+++ case_three.js
@@ -1,1 +1,11 @@
-let object = {}
+const object = {
+    firstName: 'Dewi',
+    lastName: 'Anggraini',
+    occupation: 'Admin',
+};
+
+object.hobby = 'masak';
+delete object.occupation;
+
+
+console.log(object);
