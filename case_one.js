c7921e5ac2f606cd000d849a5c9b8aba70626cc1
Vega <hello.vegaveronica@gmail.com>
2021-05-06 19:11:07 +0700

    belum selesai

--- case_one.js
+++ case_one.js
@@ -1,16 +1,43 @@
-let people  = ["Greg", "Mary", "Devon", "James"] //  <= ini variabel
+let people = ["Greg", "Mary", "Devon", "James"]; //  <= ini variabel
 // variabel people  menyimpan array
 // cara mengakses index di dalam array
 // tanda [] ini untuk memanggil index nomor berapa contoh  people[0] sama dengan Greg
 
-console.log(people[0])
+// //Case 1, line1 console.log smua orang
+// for (let i of people) {
+//   console.log(i);
+// }
 
-// kita mau loop yang ada di variable tersebut
-for (var i=0;i < people.lenght;i++){ // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
-        console.log(i) // yang aku print di terminal itu i
-}
+// Case 1 hapusgreg
+// people.splice(0, 1);
+// console.log(people);
 
+// Case 1 hapusjames
+// people.splice(3, 4);
+// console.log(people);
 
+// Case 1 add Matt to the front array
+// const x = ["Matt"];
+// x.push("Greg", "Mary", "Devon", "James");
+// console.log(x);
 
+// Case 1 add Vega to the end array
+// const x = ["Matt", "Greg", "Mary", "Devon", "James"];
+// const length = x.length;
+// x[length] = "Vega";
+// console.log(x);
 
+// Case 1 mary exit from loop *sala keknya
+// const x = ["Matt", "Greg", "Mary", "Devon", "James", "Vega"];
+// console.log(x);
+// const index = x.indexOf("Mary");
+// if (index > -1) {
+//   x.splice(index, 1);
+// }
+// console.log(x);
 
+// Case 1  not include mary or matt
+const x = ["Matt", "Greg", "Mary", "Devon", "James", "Vega"];
+console.log(x.slice(1, 6));
+
+// belum selesai
