f747cc2075d95e148c849e9e2b31ab6ed4fc73a9
nicolaussatria <nicolaus.satria1@gmail.com>
2021-05-06 22:07:37 +0700

    case1

--- case_one.js
+++ case_one.js
@@ -1,16 +1,64 @@
-let people  = ["Greg", "Mary", "Devon", "James"] //  <= ini variabel
-// variabel people  menyimpan array
-// cara mengakses index di dalam array
-// tanda [] ini untuk memanggil index nomor berapa contoh  people[0] sama dengan Greg
+let people = ["Greg", "Mary", "Devon", "James"];
 
-console.log(people[0])
-
-// kita mau loop yang ada di variable tersebut
-for (var i=0;i < people.lenght;i++){ // i itu sama dengan nol, jika i kurang dari panjang index array, i akan bertambah
-        console.log(i) // yang aku print di terminal itu i
+// people.shift();
+// people.pop();
+// people.unshift("Matt");
+// people.push("Nico");
+// console.log(people[i]);
+for(let i =0; i< people.length; i++){
+    console.log(people[i]);
 }
 
+const removeName = (name) => {
+  const x = people.filter((xaxa) => xaxa !== name);
+  return x;
+};
+console.log(removeName("Greg"));
 
+const removeName2 = (name) => {
+  const x = people.filter((xaxa) => xaxa !== name);
+  return x;
+};
+console.log(removeName("James"));
+
+const addName = (name) => {
+    // people.push(addName);
+    // console.log(addName("Matt"));
+    console.log([name, ...people]);
+  };
+addName("Matt");
+
+
+const addName2 = (name) => {
+  // people.push(addName);
+  // console.log(addName("Matt"));
+  console.log([people, ...name]);
+};
+addName("Nico");
+
+// stop when meet mary
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+  if (people[i] == "Mary") break;
+}
+
+//hapus Marry or Matt
+const arrSlice = () => {
+  const newArr = ["Greg", "Mary", "Devon", "James", "Matt"];
+  console.log(newArr.slice(0, 4));
+};
+
+arrSlice();
+
+//location Marry
+console.log(people.indexOf("Mary"));
+
+//
+console.log(people.indexOf("foo"));
+
+//
+const newArr = people.splice(2, 1, "Elizabeth", "Artie");
+console.log(people);
 
 
 
