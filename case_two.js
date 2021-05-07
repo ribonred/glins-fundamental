f1097c22f5ae9f10a526b12832f88269fe092e2f
Asdar Palari <asdarxx@gmail.com>
2021-05-07 11:38:38 +0800

    done

--- case_two.js
+++ case_two.js
@@ -1,28 +1,50 @@
+const hege = ["Cecilie","Cecilie", "Lone","Linus","Linus","totok","Budi","Susan","Susan","Susan","Budi","Iman","Raka","Linus"];
+const stale = ["Emil", "Tobias", "Linus","Budi","Iman","Susan"];
 
-
-
-var hege = ["Cecilie", "Lone","Linus","totok"];
-var stale = ["Emil", "Tobias", "Linus"];
-
-function removeElement(array, elem) {
-    var index = array.indexOf(elem);
-    if (index > -1) {
-        return array.splice(0,index);
-    }else{
-        return new Array
+function cekDiff(array1,array2){
+   // let res=[];
+    for (let i=0; i<array1.length;i++){
+        for (let j=0; j<array2.length;j++){
+           // console.log(array1[i],array2[j]);
+            if (array1[i]===array2[j]){  
+                array1.splice(i,1,''); 
+              // console.log(array1);
+              // console.log(array1.filter(xxx => xxx !== array2[j]));
+              // res.push(array[i])
+               //res = [...res, array[i]];  
+        }
+        }
     }
+  // console.log(array1);
+  //console.log(array1.filter(xx => xx !==''));
+   result = [...new Set(array1)];
+  // console.log(result);
+   result=(result.filter(xx =>xx !==''));
+   console.log(result);
 }
 
+cekDiff(hege,stale);
 
-function arrayDiff(arrayA, ArrayB) {
-    for(var i=0;i<ArrayB.length;i++){
-       var result = removeElement(arrayA,ArrayB[i])
-      if(result.length > 0){
-          return result
-      }
-    }
+
+// function removeElement(array, elem) {
+//     var index = array.indexOf(elem);
+//     if (index > -1) {
+//         return array.splice(0,index);
+//     }else{
+//         return new Array
+//     }
+// }
+
+
+// function arrayDiff(arrayA, ArrayB) {
+//     for(var i=0;i<ArrayB.length;i++){
+//        var result = removeElement(arrayA,ArrayB[i])
+//       if(result.length > 0){
+//           return result
+//       }
+//     }
    
-    }
+//     }
    
 
-console.log(arrayDiff(hege,stale))
+// console.log(arrayDiff(hege,stale));
