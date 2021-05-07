aa8c8e0948d88ec78dd4c0529f4ddf1dc554ee50
Elok-Saputra <sapoetraellok@gmail.com>
2021-05-07 14:39:39 +0800

    case three done

--- case_three.js
+++ case_three.js
@@ -1,1 +1,25 @@
-let object = {}
+let bio = {
+    firstName: 'Elok',
+    lastName: 'Saputra',
+    occupation: 'Mobile Developer'
+}
+
+//dot notation
+console.log('---dot notation---')
+console.log(`Hello I am ${bio.firstName} ${bio.lastName}, and I want to be a ${bio.occupation}`)
+
+//bracket notation
+console.log('---bracket notation---')
+console.log(`Hello I am ${bio['firstName']} ${bio['lastName']}, and I want to be a ${bio['occupation']}`)
+
+//add my hobby
+console.log('---add my hobby---')
+bio.hobby = 'Badminton'
+console.log(bio)
+
+//remove occupation
+console.log('---delete occupation---')
+delete bio.occupation
+console.log(bio)
+
+
