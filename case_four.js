d65ca3fada9245962d85950b8cf96a78da099b30
Elok-Saputra <sapoetraellok@gmail.com>
2021-05-07 20:53:17 +0800

    case four

--- case_four.js
+++ case_four.js
@@ -10,4 +10,22 @@ console.log(namesAndHobbies)
 // module.exports ={
 //     namesAndHobbies
 // }
- 
+let key = Object.keys(namesAndHobbies)
+let value = Object.values(namesAndHobbies)
+let entries = Object.entries(namesAndHobbies)
+
+for (let [namesAndHobbies, count] of entries) {
+    console.log(`${count} => ${namesAndHobbies}`)
+}
+
+namesAndHobbies.elok = 'badminton'
+console.log(namesAndHobbies)
+
+const keyExist = () => {
+    if (namesAndHobbies.hasOwnProperty('elok')) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(keyExist())
