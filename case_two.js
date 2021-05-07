a5a1620af4c99d9e6d64c695401343152e2acdb5
Elok-Saputra <sapoetraellok@gmail.com>
2021-05-08 01:01:20 +0800

    case 2 done

--- case_two.js
+++ case_two.js
@@ -3,10 +3,28 @@
 
 // console.log(a)
 
-let names = ['Mike', 'Matt', 'Nancy', 'Adam', 'Jenny', 'Nancy', 'Carl', 'Nancy'];
 
-let names2 = ['Matt', 'Shinoda', 'Shinoda', 'Chester',]
 
-let dup = [...new Set(names)];
-let dup2 = [...new Set(names2)]
-console.log(dup, dup2);
+// let names2 = ['Matt', 'Shinoda', 'Shinoda', 'Chester',]
+
+// let dup = [...new Set(names)];
+// let dup2 = [...new Set(names2)]
+// console.log(dup, dup2);
+let names = ['Matt', 'Nancy', 'Adam', 'Jenny', 'Nancy', 'Nancy'];
+
+const findDups = (names) => {
+    const dups = []
+    const compare = ['Matt', 'Shinoda', 'Chester']
+
+    for (let index of names) {
+        if (!compare.includes(index)) {
+            compare.push(index)
+        }
+        else {
+            dups.push(index)
+        }
+    }
+    return compare
+}
+
+console.log(findDups(names))
