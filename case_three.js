4a4f0e07b7fcbdd36baf16cbb9f9fa2849883495
Elok-Saputra <sapoetraellok@gmail.com>
2021-05-07 16:07:12 +0800

    case three revised

--- case_three.js
+++ case_three.js
@@ -1,25 +1,37 @@
-let bio = {
-    firstName: 'Elok',
-    lastName: 'Saputra',
-    occupation: 'Mobile Developer'
+// create object
+console.log('----creating object----')
+function Biodata(firstName, lastName, occupation) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.occupation = occupation
 }
 
+let myBio = new Biodata('Elok', 'Saputra', 'Mobile Developer')
+console.log(myBio)
+
 //dot notation
-console.log('---dot notation---')
-console.log(`Hello I am ${bio.firstName} ${bio.lastName}, and I want to be a ${bio.occupation}`)
+console.log('----dot notation----')
+console.log(`Hello my name is ${myBio.firstName} ${myBio.lastName}, i want to be a ${myBio.occupation}`)
+
+console.log('-----------------------')
+console.log('-----------------------')
 
 //bracket notation
-console.log('---bracket notation---')
-console.log(`Hello I am ${bio['firstName']} ${bio['lastName']}, and I want to be a ${bio['occupation']}`)
+console.log('----bracket notation----')
+console.log(`Hello my name is ${myBio['firstName']} ${myBio['lastName']}, i want to be a ${myBio['occupation']}`)
 
-//add my hobby
-console.log('---add my hobby---')
-bio.hobby = 'Badminton'
-console.log(bio)
+console.log('-----------------------')
+console.log('-----------------------')
+
+//adding my hobby
+console.log('----adding my hobby----')
+myBio.hobby = "Badminton"
+console.log(myBio)
+
+
 
 //remove occupation
-console.log('---delete occupation---')
-delete bio.occupation
-console.log(bio)
-
+console.log('----removing occupation----')
+delete myBio.occupation
+console.log(myBio)
 
