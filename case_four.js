df3d1996f4f1223f5fb56fd6485031f88334d13a
nicolaussatria <nicolaus.satria1@gmail.com>
2021-05-10 21:13:38 +0700

    push case4

--- case_four.js
+++ case_four.js
@@ -4,10 +4,29 @@ let namesAndHobbies = {
     'janey': "table building",
     'tim': "sailing"
 }
+for (let i=0; i<Object.keys(namesAndHobbies).length;i++){
+    console.log(`${Object.values(namesAndHobbies)[i]} => ${Object.keys(namesAndHobbies)[i]}`);
+        
+}
 
-export {namesAndHobbies}
+namesAndHobbies.nico = "jalan-jalan";
+console.log(namesAndHobbies);
+if (namesAndHobbies.nico) {
+    console.log(`${Object.keys(namesAndHobbies)[4]} ${namesAndHobbies.nico} `);
+
+}
 
 // module.exports ={
 //     namesAndHobbies
 // }
  
+// Given the following object below, write code to print the value then the key to the console separated by '=>':
+
+// let namesAndHobbies = { elie: "JavaScript", matt: "jogging", janey: "table building", tim: "sailing" }
+
+// Output should be:
+// JavaScript => elie
+// jogging => matt
+// table building => janey
+// sailing => tim Add a key for your name, and a value for your favorite hobby to the namesAndHobbies object.
+// Write an if statement that console.logs your name and hobby to the console if the key of your name is contained in the object.
