25377a1d77b52d03aa9ba122bd30fef7d630467d
endi96 <endifernanda1@gmail.com>
2021-05-10 21:48:43 +0700

    tugas

--- case_four.js
+++ case_four.js
@@ -5,9 +5,28 @@ let namesAndHobbies = {
     'tim': "sailing"
 }
 
-export {namesAndHobbies}
+// export { namesAndHobbies }
 
 // module.exports ={
 //     namesAndHobbies
 // }
- 
+
+function inverse(object) {
+    var hasil = {};
+    for (var key in object) {
+        hasil[object[key]] = key;
+    }
+    return hasil;
+}
+
+console.log(inverse(namesAndHobbies))
+
+namesAndHobbies.endi = 'basket'
+
+console.log(namesAndHobbies)
+
+if (namesAndHobbies.hasOwnProperty('endi')) {
+    console.log(true)
+} else {
+    console.log(false)
+}
--- case_three.js
+++ case_three.js
@@ -1,1 +1,14 @@
-let object = {}
+let object = {
+    firstName: 'Endi',
+    lastName: 'Fernanda',
+    occupation: 'student'
+}
+
+console.log(object.firstName)
+console.log(object['firstName'])
+
+console.log(object.lastName)
+console.log(object['lastName'])
+
+console.log(object.occupation)
+console.log(object['occupation'])
