efa17064c515b422d5ea643b1686cf0d8979f93f
nicolaussatria <nicolaus.satria1@gmail.com>
2021-05-10 20:46:32 +0700

    case all

--- case_three.js
+++ case_three.js
@@ -1,1 +1,11 @@
-let object = {}
+// let object = {}
+let dataDiri = {
+    firstName: "nicolaus",
+    lastName: "satria",
+    occupation: "Bekasi",
+  }
+  console.log(dataDiri);
+  delete dataDiri.occupation; //hapus okupasi
+  console.log(dataDiri);
+  dataDiri.hobby = "travelling"; //nambah gobi
+  console.log(dataDiri);
